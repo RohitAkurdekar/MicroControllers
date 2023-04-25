6995ddca751a56f2707667cbe42971b12bf105ed
Rohit Akurdekar <114896370+RohitAkurdekar@users.noreply.github.com>
2023-04-25 23:32:40 +0530

    Button and LED

--- ESP32/ESP_Python/ButtonLED/ButtonLED.py
+++ ESP32/ESP_Python/ButtonLED/ButtonLED.py
@@ -16,9 +16,12 @@ Brief   : Turn on onboard LED when onboard button is pressed.
 Tested  : OK
 '''
 def ledOnbuttonPress(led=2,button=0):
-    LED = Pin(led,Pin.OUT)
-    BUTTON = Pin(button,Pin.IN)
+    LED = Pin(led,Pin.OUT)              # Configure LED pin as output pin 
+    BUTTON = Pin(button,Pin.IN)         # Configure BUTTON pin as input pin
+    
+    # If button is pressed
     if BUTTON.value() == 0:
+        # Turn ON LED
         LED.value(1)
         print("Button pressed and LED turned ON")
     else:
